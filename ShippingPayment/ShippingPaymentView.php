ffea90e63c992d26fa7c1c6c88d09a49cd47b5fa
bobricDima <38735531+bobricDima@users.noreply.github.com>
2018-04-25 23:39:47 +0300

    п
    готов

--- ShippingPayment/ShippingPaymentView.php
+++ ShippingPayment/ShippingPaymentView.php
@@ -1,8 +1,15 @@
-<div class="container-fulid payment-shipping">
-
-	<div class="container">
-			<div class="row ">
-			<h2 class="section-header">Доставка и Оплата</h2> 
-			</div>
-	</div>
+<div class="container-fluid">
+  <div class="container">
+    <div class="row">
+      <h2 class="section-header">Доставка и оплата</h2>
+    </div>
+   <div class="row">
+     <h3 class="selection">Выбор и заказ очисной станции</h3>
+   </div>
+   <div class="row">
+     <p class="selection-item-text">
+       Определите модель септика 
+     </p>
+   </div>
+  </div>
 </div>
