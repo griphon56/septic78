d8c14f1cbe20ed63b3529aa26c308944fd8b4f82
user-ПК\user <griphon56@gmail.com>
2018-05-01 00:55:35 +0300

    ShippingPayment

---
+++ ShippingPayment/requestForm/requestFormView.php
@@ -0,0 +1,39 @@
+<div class="container-fluid shipping-pay-form contact-form">
+ <div class="shipping-pay-form-bg-img">
+   <div class="container">
+    <div class="row justify-content-end">
+      <div class="col-md-8">
+        <h3 class="section-item">ОСТАВЬТЕ ЗАЯВКУ</h3>
+        <form action="#" id="shipping-pay-form">
+          <div class="row">
+            <div class="col-6">
+              <input type="text" placeholder="Имя">
+            </div>
+            <div class="col-6">
+              <input type="text" placeholder="Телефон">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+               <div class="message-container">
+              <textarea  class="message" name="message"></textarea>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12">
+              <div class="shipping-pay-contact-seal">
+                <img src="../img/contactView/locked-padlock.png"alt="" alt="">
+                <p>Мы гарантируем конфеденциальность ваших данных</p>
+              </div>
+            </div>
+          </div>
+          <div class="row justify-content-md-center">
+            <div class="col-md-5"><input type="submit" value="Отправить" class="btn"></div>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  </div>
+</div>  
