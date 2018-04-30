dcd5804018b8171280084c98d278cbb58ad6cf37
bobricDima <38735531+bobricDima@users.noreply.github.com>
2018-04-30 19:37:47 +0300

    Page is Maded

--- Form/FormView.php
+++ Form/FormView.php
@@ -1,39 +1,29 @@
 <div class="container-fluid">
     <div class="row ">
-      <div class="captioin">
-         <div class="item">
-           <h5 class="caption-item">
-             Оставьте заявку 
-          </h5>
-          <div class="caption-item-2">
-            <div class="row">
-                 <div class="col-6">
-                   <form action="">
-                      <input id="first-name"type="text" placeholder="Имя" >
-                  </form>
-                 </div>
-                 
-                <div class="col-6">
-                  <form action="">
-                      <input type="text" id="phone" placeholder="Телфон">
-                  </form>
-                </div>
-            </div>
-            <div class="row">
-           <div class="caption-item-3">
+     <div class="caption">
+       <h5>ОСТАВЬТЕ ЗАЯВКУ</h5>
+        <div class="row">
+          <div class="col-6">
             <form action="">
-                      <textarea name="Ваше сообщение" id="massage" cols="90" rows="10"></textarea>
-                  </form>
-              </div>
-            </div>
+              <input class="first_name" type="text" placeholder="Имя">
+            </form>
           </div>
-        
-          
-          
+          <div class="col-6">
+            <form action="" >
+              <input type="text" class="first_name" placeholder="Телефон">
+            </form>
+          </div>
+        </div>
+        <div class="row">
+          <form action="" id="item-text">
+            <textarea  id="massage" name="Ваше сообщение"  cols="92" rows="7">Ваше Сообщение</textarea>
+          </form>
+        </div>
+         
+       </div>
+     </div>
     </div>  
-  </div>    
-</div>
-</div>
+   
 
 
  
