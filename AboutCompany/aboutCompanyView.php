282b79f33536ee625cf62374ac635a0b3eea88a8
alinamorneva <alina.morneva@mail.ru>
2018-05-03 20:35:58 +0300

    aboutcompany

--- AboutCompany/aboutCompanyView.php
+++ AboutCompany/aboutCompanyView.php
@@ -1,9 +1,47 @@
 <div class="container-fluid about-company">
-  <div class="container">
-    <div class="row">
-      <div class="col-md-5">
-        <h2>О компании</h2>
-      </div>
-    </div>
-  </div>
+	<div class="container">
+		<div class="row col">
+	        <h2 class="section-header">О компании</h2>
+	    </div>
+		<div class="row section-item-text">
+	        <div class="col-md-11">
+	          	<p>
+	            Наша организация является частью группы строительных компаний, которая ведет свою историю с 1994 г. За годы существования наша фирма закрепляла успешные позиции на рынке Санкт-Петербурга и Ленинградской области, а так же и в других регионах вплоть до Дальнего Востока. Так же в текущее время идет подготовка для работы на рынке США.
+	            Наша Компания оказывает строительные услуги и продает строительные товары.
+	          	</p>
+	        </div>
+		</div>
+		<div class="row col">
+	        <h3 class="section-item">Партнеры</h3>
+	  	</div>
+		<div class="row section-item-text">
+	        <div class="col-md-11">
+	          	<p>
+	            Нашими Партнерами и клиентами являются ПАО "ГАЗПРОМ", ТЭК "МОСЭНЕРГО", ОАО "Западный скоростной диаметр", Комитет Госзаказа Ленинградской области, АО «Райффайзенбанк», ПАО «Сбербанк», ПАО «Альфабанк», а так же частные лица.
+	            Наша компания является дилером всех наиболее крупных и известных производителей. Это подтверждается наличием прямых договоров. Мы постоянно растем и развиваемся, чтобы быть рядом, когда понадобится наша помощь, опыт и знания! Мы предлагаем не просто оборудование - мы предлагаем законченное решение конкретной задачи! Мы всегда рады новым партнерам и открыты для сотрудничества!
+	          	</p>
+	        </div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<img src="../img/aboutView/water.png" alt="icon" class="about-img">
+			</div>
+		</div>
+	  	<div class="row col">
+		    <h3 class="section-item">Сертификаты и дипломы</h3>
+		</div>
+
+		<div class="about-bg-img">
+			<div class="row position-img">
+				<div class="col-md-6">
+					<img src="../img/aboutView/1.jpg" alt="icon" class="about-sertific-img">
+					<img src="../img/aboutView/2.jpg" alt="icon" class="about-sertific-img y-class">
+				</div>
+				<div class="col-md-6">
+					<img src="../img/aboutView/3.jpg" alt="icon" class="about-sertific-img y-class">
+					<img src="../img/aboutView/4.jpg" alt="icon" class="about-sertific-img">
+				</div>
+			</div>
+	  	</div>
+	</div>
 </div>
