6a859dbcea5e6510582c487688a46245a006d5a6
griphon56 <griphon56@gmail.com>
2018-07-15 16:35:38 +0300

    Search module

--- View/Search/searchView.php
+++ View/Search/searchView.php
@@ -1,10 +1,12 @@
 <?php
   include(USER_TEMPLATE.'Search/actionSearch.php');
+
+  $s_search = $_REQUEST['s_search'] ?? '';
 ?>
 
 <div class="search-input">
   <div class="box-search">
-    <input type="search" id="s_search" placeholder="Поиск по товарам..." value="" />
+    <input type="search" id="s_search" placeholder="Поиск по товарам..." value="<?=$s_search?>" />
     <img src="<?=VIEW?>img/headerView/magnifying-glass.png" id="search" alt="" />
   </div>
 </div>
