5f379c30c52162e25b0e4807eed64252aef15294
alinamorneva <alina.morneva@mail.ru>
2018-07-15 11:12:25 +0300

    Картинки произвоителей выравнивание

--- View/Home/homeSliderBrandView.php
+++ View/Home/homeSliderBrandView.php
@@ -1,22 +1,28 @@
 <div class="container">
   <div class="autoplay-slick-slider row align-items-center">
     <div class="col slide-brand-item">
-      <img src="<?=VIEW?>img/homeView/astra-logo_150x50_96a.jpg" alt="" class="slide-brand-img">
+      <img src="<?=VIEW?>img/homeView/ros.png" alt="" class="slide-brand-img">
     </div>
     <div class="col slide-brand-item">
-      <img src="<?=VIEW?>img/homeView/Deka-logo_150x50_1aa.png" alt="" class="slide-brand-img">
+      <img src="<?=VIEW?>img/homeView/evrobion.png" alt="" class="slide-brand-img">
     </div>
     <div class="col slide-brand-item">
-      <img src="<?=VIEW?>img/homeView/eurolos-logo_150x50_1aa.png" alt="" class="slide-brand-img">
+      <img src="<?=VIEW?>img/homeView/evrolos.png" alt="" class="slide-brand-img">
     </div>
     <div class="col slide-brand-item">
-      <img src="<?=VIEW?>img/homeView/logo-eurobion2_150x50_96a.jpg" alt="" class="slide-brand-img">
+      <img src="<?=VIEW?>img/homeView/vor.png" alt="" class="slide-brand-img">
     </div>
     <div class="col slide-brand-item">
-      <img src="<?=VIEW?>img/homeView/rostok-logo_150x50_96a.jpg" alt="" class="slide-brand-img">
+      <img src="<?=VIEW?>img/homeView/grand.png" alt="" class="slide-brand-img">
     </div>
     <div class="col slide-brand-item">
-      <img src="<?=VIEW?>img/homeView/vortex-logo_150x50_96a.jpg" alt="" class="slide-brand-img">
+      <img src="<?=VIEW?>img/homeView/deka.png" alt="" class="slide-brand-img">
+    </div>
+    <div class="col slide-brand-item">
+      <img src="<?=VIEW?>img/homeView/sbm.png" alt="" class="slide-brand-img">
+    </div>
+    <div class="col slide-brand-item">
+      <img src="<?=VIEW?>img/homeView/topol.png" alt="" class="slide-brand-img">
     </div>
   </div>
 </div>
