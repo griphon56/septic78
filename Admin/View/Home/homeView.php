3a6a8bea53e6d213ea9502db16f1937071fef9b1
griphon56 <griphon56@gmail.com>
2018-07-26 19:29:14 +0300

    update product

--- Admin/View/Home/homeView.php
+++ Admin/View/Home/homeView.php
@@ -8,16 +8,16 @@
       <h3 class="section-item">Панель управления</h3>
     </div>
   </div>
-<!--  <div class="row">-->
-<!--    <div class="col-md-3">-->
-<!--      <form action="" method="post">-->
-<!--        <input-->
-<!--          type="submit"-->
-<!--          value="update_product"-->
-<!--          name="action_form"-->
-<!--          class="btn"-->
-<!--        >-->
-<!--      </form>-->
-<!--    </div>-->
-<!--  </div>-->
+  <div class="row">
+    <div class="col-md-3">
+      <form action="" method="post">
+        <input
+          type="submit"
+          value="update_product"
+          name="action_form"
+          class="btn"
+        >
+      </form>
+    </div>
+  </div>
 </div>
