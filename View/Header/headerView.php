7476070579ab7f24fd001f8ac0748e2ec74d4993
griphon56 <griphon56@gmail.com>
2018-08-02 15:11:20 +0300

    mobile header (search)

--- View/Header/headerView.php
+++ View/Header/headerView.php
@@ -2,6 +2,8 @@
 <html lang="ru">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width; initial-scale=1.0">
+
   <title><?=TITLE?></title>
 
   <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
