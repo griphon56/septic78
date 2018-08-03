f9f74fde79eb4e15898374b2b2c909ac309fef19
alinamorneva <alina.morneva@mail.ru>
2018-08-03 16:24:35 +0300

    aboutcomp

--- View/AboutCompany/aboutCompanyView.php
+++ View/AboutCompany/aboutCompanyView.php
@@ -20,21 +20,21 @@
     </div>
     <div class="row">
       <div class="col-md-12">
-        <img src="<?=VIEW?>img/aboutView/water.png" alt="icon" class="about-img">
+        <img src="<?=VIEW?>img/aboutView/water.png" alt="icon" class="about-img m-about-img">
       </div>
     </div>
   </div>
 </div>
 
 
-<div class="about-bg-img">
+<div class="about-bg-img m-about-bg-img">
   <div class="container-fluid">
     <div class="container" id="container">
       <div class="row col">
         <h3 class="section-item">Сертификаты и дипломы</h3>
       </div>
-      <div class="row position-img swipebox">
-        <div class="col-md-6" id="mode-gallery">
+      <div class="row position-img m-position-img swipebox">
+        <div class="col-md-6 m-about-sertific-img" id="mode-gallery">
           <a href="<?=VIEW?>img/aboutView/1.jpg" rel="gallery-1" class="swipebox">
             <img src="<?=VIEW?>img/aboutView/1.jpg" alt="icon" class="about-sertific-img">
           </a>
@@ -42,7 +42,7 @@
             <img src="<?=VIEW?>img/aboutView/2.jpg" alt="icon" class="about-sertific-img y-class">
           </a>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 m-about-sertific-img">
           <a href="<?=VIEW?>img/aboutView/3.jpg" rel="gallery-2" class="swipebox">
             <img src="<?=VIEW?>img/aboutView/3.jpg" alt="icon" class="about-sertific-img y-class">
           </a>
@@ -51,8 +51,8 @@
           </a>
         </div>
       </div>
-      <div class="row position-img swipebox">
-        <div class="col-md-6">
+      <div class="row position-img m-position-img swipebox">
+        <div class="col-md-6 m-about-sertific-img">
           <a href="<?=VIEW?>img/aboutView/5.jpg" rel="gallery-3" class="swipebox">
             <img src="<?=VIEW?>img/aboutView/5.jpg" alt="icon" class="about-sertific-img">
           </a>
@@ -60,7 +60,7 @@
             <img src="<?=VIEW?>img/aboutView/6.jpg" alt="icon" class="about-sertific-img y-class">
           </a>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-6 m-about-sertific-img">
           <a href="<?=VIEW?>img/aboutView/7.jpg" rel="gallery-4" class="swipebox">
             <img src="<?=VIEW?>img/aboutView/7.jpg" alt="icon" class="about-sertific-img y-class">
           </a>
