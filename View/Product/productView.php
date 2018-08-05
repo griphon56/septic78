829cfe44d90315dd606c2b5b30c85262d2223761
griphon56 <griphon56@gmail.com>
2018-08-05 20:32:34 +0300

    mobile single product

--- View/Product/productView.php
+++ View/Product/productView.php
@@ -62,7 +62,7 @@
               <a href="#" onclick="add_to_cart_single(this); return false;" name="<?=$a_product['k_product']?>" class="btn btn-info product-single-btn">Заказать</a>
             </div>
             <div class="col-4 col-sm-4 col-md-4">
-              <div class="quantity">
+              <div class="quantity m-product-qty">
                 <input type="number" min="1" max="9" step="1" value="1" name="qty_product">
               </div>
             </div>
