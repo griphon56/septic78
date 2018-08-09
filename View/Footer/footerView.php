52bd497e748730460c1ed8e1940f01dc205c2a57
griphon56 <griphon56@gmail.com>
2018-08-10 02:09:41 +0300

    optimization

--- View/Footer/footerView.php
+++ View/Footer/footerView.php
@@ -107,8 +107,13 @@
 </div>
 
 </div>
+  <link href="<?=VIEW?>style/desktop/main.css" property="stylesheet" rel="stylesheet">
 
-<script src="<?=VIEW?>js/slick.js"></script>
-<script src="<?=VIEW?>js/main.js"></script>
+  <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.0.4/popper.js"></script>
+  <script src="<?=VIEW?>js/bootstrap.min.js"></script>
+  <script src="<?=VIEW?>js/jquery.swipebox.js"></script>
+  <script src="<?=VIEW?>js/slick.js"></script>
+  <script src="<?=VIEW?>js/main.js"></script>
 </body>
 </html>
--- View/Header/headerView.php
+++ View/Header/headerView.php
@@ -6,15 +6,9 @@
 
   <title><?=TITLE?></title>
 
-  <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.0.4/popper.js"></script>
-  <script src="<?=VIEW?>js/bootstrap.min.js"></script>
-  <script src="<?=VIEW?>js/jquery.swipebox.js"></script>
-
   <link rel="icon" href="<?=VIEW?>img/headerView/favicon.png" type="image/x-icon">
   <link rel="shortcut icon" href="<?=VIEW?>img/headerView/favicon.png" type="image/x-icon">
 
-  <link href="<?=VIEW?>style/main.css" rel="stylesheet">
 </head>
 <body>
 <div class="wrapper">
