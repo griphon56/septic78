4c70bb6fbaacb8744b962d203d33c36d03d71ca8
griphon56 <griphon56@gmail.com>
2018-08-11 17:49:03 +0300

    optimization

--- Admin/View/Footer/footerView.php
+++ Admin/View/Footer/footerView.php
@@ -1,5 +1,11 @@
   </div>
 
+  <link href="<?=VIEW?>style/desktop/main.css" rel="stylesheet">
+
+  <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.0.4/popper.js"></script>
+  <script src="<?=VIEW?>js/bootstrap.min.js"></script>
+  <script src="<?=VIEW?>js/jquery.swipebox.js"></script>
   <script src="<?=VIEW?>js/slick.js"></script>
   <script src="<?=VIEW?>js/main.js"></script>
 
--- Admin/View/Header/headerView.php
+++ Admin/View/Header/headerView.php
@@ -5,14 +5,8 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link href="<?=VIEW?>style/main.css" rel="stylesheet">
 
   <title>Septic5 - Administration</title>
-
-  <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.0.4/popper.js"></script>
-  <script src="<?=VIEW?>js/bootstrap.min.js"></script>
-  <script src="<?=VIEW?>js/jquery.swipebox.js"></script>
 
   <link rel="icon" href="<?=VIEW?>img/headerView/favicon.png" type="image/x-icon">
   <link rel="shortcut icon" href="<?=VIEW?>img/headerView/favicon.png" type="image/x-icon">
