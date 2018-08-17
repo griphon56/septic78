7031ec358741926f3b5f3b2c81b175a2736c86b7
griphon56 <griphon56@gmail.com>
2018-08-17 18:51:47 +0300

    #53 страницы ошибок

---
+++ View/ErrorPage/403.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+  <title>Септик5.рф</title>
+  <link rel="icon" href="View/img/headerView/favicon.png" type="image/x-icon">
+  <link rel="shortcut icon" href="View/img/headerView/favicon.png" type="image/x-icon">
+  <link href="View/style/desktop/errorPage.css" property="stylesheet" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+  <div class="box">
+    <h1 class="error-title-403">403</h1>
+    <h2 class="error-desc">Forbidden</h2>
+    <h2 class="error-desc error-desc-tip">(Ход закрыт)</h2>
+  </div>
+</div>
+</body>
+</html>
--- View/ErrorPage/forbidden.php
+++
@@ -1,2 +0,0 @@
-<h1 class="notfound-title">403</h1>
-<h2 class="notfound-desc">Ход закрыт</h2>
